ab70e23e5d37dc53e674ccd87d59c127c15b3d19
Abdqadr1 <abolarinwaquadri@gmail.com>
2021-08-26 08:31:09 +0800

    done for now

--- functions/index.js
+++ functions/index.js
@@ -1,64 +1,19 @@
 /* eslint-disable max-len */
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const algorithm = require("aes256");
 // const eccrypto = require("eccrypto");
 const crypto = require("crypto");
 
 // Get the `FieldValue` object
 admin.initializeApp();
 
+const keys = admin.firestore().collection("keys");
+const staffs = admin.firestore().collection("staffs");
+const admins = admin.firestore().collection("admins");
+const students = admin.firestore().collection("students");
+const thesisCollection = admin.firestore().collection("thesis");
 
-exports.addCustomClaims = functions.https.onCall((data, context) => {
-  // Checking that the user is authenticated.
-  if (!context.auth) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError("failed-precondition", "The function must be called " +
-        "while authenticated.");
-  }
-
-  if (!(data.which == "staff" || data.which == admin)) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError("permission-denied", "You have to be a staff or admin to perform this action");
-  }
-  const uid = data.uid;
-  const role = data.role;
-  const value = data.value;
-  const email = data.email;
-
-  if (!(typeof(uid) === "string") || uid.length === 0) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError("invalid-argument', 'The function must be called " +
-        "with a user id.");
-  }
-
-  if (!(typeof(role) === "string") || role.length === 0) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError("invalid-argument", "The function must be called " +
-        "with a role.");
-  }
-
-  if (!(typeof(email) === "string") || email.length === 0) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError("invalid-argument", "The function must be called " +
-        "with a user email.");
-  }
-
-  if (!(typeof(value) === "boolean")) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError("invalid-argument", "The function must be called " +
-        "with a boolean value.");
-  }
-
-
-  return admin.auth().setCustomUserClaims(uid, {
-    [role]: value,
-  }).then(() => {
-    return {
-      message: `Request fulfilled! ${email} is now a
-                ${role}.`,
-    };
-  });
-});
 
 exports.deleteUser = functions.https.onCall((data, context) => {
   if (!context.auth && context.auth.token.admin !== true) {
@@ -74,10 +29,8 @@ exports.deleteUser = functions.https.onCall((data, context) => {
     return {
       message: "User deleted",
     };
-  }).catch((error) => {
-    return {
-      message: "Error deleting user" + error,
-    };
+  }).catch(() => {
+    throw new functions.https.HttpsError("aborted", "error occurred while deleting user");
   });
 });
 
@@ -89,9 +42,6 @@ exports.createUserRecord = functions.https.onCall(async (data, context) => {
   if (!(typeof (uid) === "string") || uid.length === 0) {
     throw new functions.https.HttpsError("invalid-argument", "Function must be called with a valid user id");
   }
-  const keys = admin.firestore().collection("keys");
-  const staffs = admin.firestore().collection("staffs");
-  const students = admin.firestore().collection("students");
 
   const userKeys = crypto.createECDH("secp256k1");
   userKeys.generateKeys();
@@ -102,7 +52,7 @@ exports.createUserRecord = functions.https.onCall(async (data, context) => {
 
   if (data.which == "student") {
     const studentRecord = {
-      "matric": encryptData(data.matric, publicKey),
+      "matric": data.matric,
       "name": encryptData(data.name, publicKey),
       "email": encryptData(data.email, publicKey),
       "phone": encryptData(data.phone, publicKey),
@@ -117,26 +67,32 @@ exports.createUserRecord = functions.https.onCall(async (data, context) => {
       message: ` ${data.email} record is added`,
     };
   }
-  if (data.which == "staff") {
+  if (data.which == "staff" || data.which == "admin") {
     const staffRecord = {
-      "staff_id": encryptData(data.staff_id, publicKey),
+      "staff_id": data.staff_id,
       "name": encryptData(data.name, publicKey),
       "email": encryptData(data.email, publicKey),
       "phone": encryptData(data.phone_no, publicKey),
       "title": encryptData(data.title, publicKey),
       "office_address": encryptData(data.office_address, publicKey),
-      "dept": encryptData(data.dept, publicKey),
       "public_key": publicKey,
     };
-    await staffs.doc(uid).set(staffRecord);
-    const role = data.role;
-    const value = data.value;
-    await admin.auth().setCustomUserClaims(uid, {
-      [role]: value,
-    });
+    if (data.which == "staff") {
+      staffRecord.dept = encryptData(data.dept, publicKey);
+      await staffs.doc(uid).set(staffRecord);
+      await admin.auth().setCustomUserClaims(uid, {
+        [data.which]: true,
+      });
+    }
+    if (data.which == "admin") {
+      await admins.doc(uid).set(staffRecord);
+      await admin.auth().setCustomUserClaims(uid, {
+        [data.which]: true,
+      });
+    }
     return {
       message: `Request fulfilled! ${data.email} is now a
-                ${role}.`,
+                ${data.which}.`,
     };
   }
 });
@@ -145,8 +101,6 @@ exports.getUserDocument = functions.https.onCall(async (data, context) => {
   if (!context || !context.auth.uid) {
     throw new functions.https.HttpsError("unauthenticated", "You are not authenticated to perform this action");
   }
-  const staffs = admin.firestore().collection("staffs");
-  const students = admin.firestore().collection("students");
   const id = data.uid;
   const which = data.which;
   if (which == "staff") {
@@ -155,6 +109,14 @@ exports.getUserDocument = functions.https.onCall(async (data, context) => {
       throw new functions.https.HttpsError("not-found", "Record does not exist");
     } else {
       return decryptRecord(staffRecord.data());
+    }
+  }
+  if (which == "admin") {
+    const adminRecord = await admins.doc(id).get();
+    if (!adminRecord.exists) {
+      throw new functions.https.HttpsError("not-found", "Record does not exist");
+    } else {
+      return decryptRecord(adminRecord.data());
     }
   }
   if (which == "student") {
@@ -172,7 +134,6 @@ exports.getStudentThesis = functions.https.onCall(async (data, context) => {
     throw new functions.https.HttpsError("unauthenticated", "You are not authenticated to perform this action");
   }
   const id = data.uid;
-  const thesisCollection = admin.firestore().collection("thesis");
   const thesis = await thesisCollection.doc(id).get();
   if (!thesis.exists) {
     throw new functions.https.HttpsError("not-found", "Record does not exist");
@@ -182,24 +143,152 @@ exports.getStudentThesis = functions.https.onCall(async (data, context) => {
   }
 });
 
+exports.registerThesis = functions.https.onCall(async (data, context) => {
+  if (!context || !context.auth.uid) {
+    throw new functions.https.HttpsError("unauthenticated", "You are not authenticated to perform this action");
+  }
+  const id = data.uid;
+  const author = await students.doc(id).get();
+  const authorData = author.data();
+  const thesisData = {
+    "title": encryptData(data.title, authorData["public_key"]),
+    "author": encryptData(data.author, authorData["public_key"]),
+    "content": encryptData(data.content, authorData["public_key"]),
+  };
+  await thesisCollection.doc(id).set(thesisData);
+  return {
+    "message": "Thesis added successfully",
+  };
+});
+
+exports.getStudentDocument = functions.https.onCall(async (data, context) => {
+  if (!context || !context.auth.uid) {
+    throw new functions.https.HttpsError("unauthenticated", "You are not authenticated to perform this action");
+  }
+  const matric = data.matric;
+  if (!matric || matric.length == 0) {
+    throw new functions.https.HttpsError("invalid-argument", "Enter a matric number to search for");
+  }
+  const studentRecord = await students.where("matric", "==", data.matric.toUpperCase()).limit(1).get();
+  if (studentRecord.size() == 1) {
+    return decryptRecord(studentRecord.data());
+  } else {
+    throw new functions.https.HttpsError("not-found", "No student with " + data.matric.toUpperCase() + " found");
+  }
+});
+
+exports.createThesisRecord = functions.https.onCall(async (data, context) => {
+  if (!context.auth && context.auth.token.admin !== true) {
+    throw new functions.https.HttpsError("unauthenticated", "You are not authenticated to perform this action");
+  }
+  const which = data.which;
+  const uid = data.uploaded_by;
+  if (which == "admin") {
+    const userRecord = await admins.doc(uid).get();
+    if (userRecord.exists) {
+      const user = decryptRecord(userRecord.data());
+      const key = userRecord.data().public_key;
+      const thesisRecord = {
+        "author_name": encryptData(data.author_name, key),
+        "author_matric": encryptData(data.author_matric, key),
+        "title": encryptData(data.title, key),
+        "uri": encryptData(data.uri, key),
+        "uploaded_by": encryptData(data.uploaded_by, key),
+        "uploaded_by_id": user.staff_id,
+        "public_key": key,
+      };
+      const res = await thesisCollection.add(thesisRecord);
+      return {
+        message: `thesis record created, ${res.id}`,
+      };
+    } else {
+      throw new functions.https.HttpsError("not-found", "Admin not found by that uid");
+    }
+  }
+
+  if (which == "student") {
+    const userRecord = await students.doc(uid).get();
+    if (userRecord.exists) {
+      const user = decryptRecord(userRecord.data());
+      const key = userRecord.data().public_key;
+      const thesisRecord = {
+        "author_name": encryptData(data.author_name, key),
+        "author_matric": encryptData(data.author_matric, key),
+        "title": encryptData(data.title, key),
+        "uri": encryptData(data.uri, key),
+        "uploaded_by": encryptData(data.uploaded_by, key),
+        "uploaded_by_id": user.matric,
+        "public_key": key,
+      };
+      const res = await thesisCollection.add(thesisRecord);
+      return {
+        message: `thesis record created, ${res.id}`,
+      };
+    } else {
+      throw new functions.https.HttpsError("not-found", "Admin not found by that uid");
+    }
+  }
+});
+
+exports.getThesis = functions.https.onCall(async (data, context) => {
+  if (!context.auth && !context.auth.uid) {
+    throw new functions.https.HttpsError("unauthenticated", "You are not authenticated to perform this action");
+  }
+  const thesis = [];
+  const uid = data.uid;
+  // const which = data.which;
+  const snapshot = await thesisCollection.where("uploaded_by_id", "==", uid).get();
+  const list = snapshot.docs;
+  if (list.length > 0) {
+    list.forEach((document) => {
+      // const id = document.id;
+      const dataEn = document.data();
+      const record = decryptThesisRecord(dataEn);
+      thesis.push(record);
+    });
+  }
+  return {thesis};
+});
+
 const encryptData = (data, key) => {
-  const algorithm = require("aes256");
   const encrypted = algorithm.encrypt(key, data);
   return encrypted;
 };
 
 const decryptData = (encrypt, key) => {
-  const algorithm = require("aes256");
   const decrypted = algorithm.decrypt(key, encrypt);
   return decrypted;
 };
 
 const decryptRecord = (record) => {
   const key = record["public_key"];
+  console.log("public_key", key);
   const newObj = {};
+  // eslint-disable-next-line guard-for-in
   for (const prop in record) {
-    if (prop !== "public_key") {
+    if (prop !== "public_key" && prop !== "matric" && prop !== "staff_id" && prop !== "uploaded_by_id") {
       newObj[prop] = decryptData(record[prop], key);
+    }
+    if (prop == "matric" || prop == "staff_id" || prop == "uploaded_by_id") {
+      newObj[prop] = record[prop];
+    }
+  }
+  return newObj;
+};
+
+const decryptThesisRecord = (thesis) => {
+  const key = thesis.public_key;
+  const newObj = {};
+  for (const prop in thesis) {
+    if (Object.prototype.hasOwnProperty.call(thesis, prop)) {
+      if (prop !== "public_key" && prop !== "uploaded_by_id") {
+        newObj[prop] = decryptData(thesis[prop], key);
+      }
+      if (prop == "uploaded_by_id") {
+        newObj[prop] = thesis[prop];
+      }
+    } else {
+      throw new functions.https.HttpsError("not-found", `"property" ${prop} not found`);
     }
   }
   return newObj;
